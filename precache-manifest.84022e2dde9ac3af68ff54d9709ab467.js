6c9ad783052bcf460ea2350fbf7f530a75be53cc
Oleksii Levchenko <>
2020-09-14 21:46:26 +0300

    Updates

---
+++ precache-manifest.84022e2dde9ac3af68ff54d9709ab467.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d28f91920135d99759f1ab360f795caf",
+    "url": "/react-hw-02-phonebook/index.html"
+  },
+  {
+    "revision": "b1537b624d12fa37eb9a",
+    "url": "/react-hw-02-phonebook/static/css/main.5fa49014.chunk.css"
+  },
+  {
+    "revision": "f6d241d2a28d6712f863",
+    "url": "/react-hw-02-phonebook/static/js/2.7e54c27f.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/react-hw-02-phonebook/static/js/2.7e54c27f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1537b624d12fa37eb9a",
+    "url": "/react-hw-02-phonebook/static/js/main.ea0be745.chunk.js"
+  },
+  {
+    "revision": "6f482f3e5b3d66793c08",
+    "url": "/react-hw-02-phonebook/static/js/runtime-main.2c5ed82f.js"
+  }
+]);
